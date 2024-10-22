1c9eeecf7327a678f5867c5f383609b7ec23680d
VoidOG <VoidGod123@outlook.com>
2024-10-22 19:24:02 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -19,31 +19,31 @@ class Config(object):
 
     # <================================================ REQUIRED ======================================================>
     # Telegram API configuration
-    API_ID = 204 # Get this value from my.telegram.org/apps
-    API_HASH = ""
+    API_ID = 22878444
+    API_HASH = "550641aa3600a98c1cb94afc259f2244"
 
     # Database configuration (PostgreSQL)
-    DATABASE_URL = "postgres:"
+    DATABASE_URL = "postgres://oaykvtmj:bsIGPV7wmId1x1CNH9eqxQVX5t25cHI3@manny.db.elephantsql.com/oaykvtmj"
 
     # Event logs chat ID and message dump chat ID
-    EVENT_LOGS = -100
-    MESSAGE_DUMP = -100
+    EVENT_LOGS = -1002183841044
+    MESSAGE_DUMP = -1002183841044
 
     # MongoDB configuration
-    MONGO_DB_URI = ""
+    MONGO_DB_URI = "mongodb+srv://Cenzo:Cenzo123@cenzo.azbk1.mongodb.net/"
 
     # Support chat and support ID
-    SUPPORT_CHAT = ""
-    SUPPORT_ID = -100
+    SUPPORT_CHAT = "Alcyone_Support"
+    SUPPORT_ID = -1002246146947
 
     # Database name
-    DB_NAME = ""
+    DB_NAME = "cenzo"
 
     # Bot token
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "7744594521:AAF1qQUMxvwtCbsYTdEBZetZ5JX2A-gcHbs"  # Get bot token from @BotFather on Telegram
 
     # Owner's Telegram user ID (Must be an integer)
-    OWNER_ID = 5907205317
+    OWNER_ID = 6663845789
     # <=======================================================================================================>
 
     # <================================================ OPTIONAL ======================================================>
