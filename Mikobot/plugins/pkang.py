a96905d107765a527e0f45d92b9c1a2931ae3c87
VoidOG <VoidGod123@outlook.com>
2024-11-01 19:30:00 +0530

    Update pkang.py

--- Mikobot/plugins/pkang.py
+++ Mikobot/plugins/pkang.py
@@ -16,18 +16,18 @@ async def _packkang(app, message):
     """
     @MaybeSuraj on telegram. who helped me in making this module.
     """
-    txt = await message.reply_text("Processing....")
+    txt = await message.reply_text("𝖯𝗋𝗈𝖼𝖾𝗌𝗌𝗂𝗇𝗀....")
     if not message.reply_to_message:
-        await txt.edit("Reply to message")
+        await txt.edit("𝖱𝖾𝗉𝗅𝗒 𝗍𝗈 𝗆𝖾𝗌𝗌𝖺𝗀𝖾")
         return
     if not message.reply_to_message.sticker:
-        await txt.edit("Reply to sticker")
+        await txt.edit("𝖱𝖾𝗉𝗅𝗒 𝗍𝗈 𝗌𝗍𝗂𝖼𝗄𝖾𝗋")
         return
     if (
         message.reply_to_message.sticker.is_animated
         or message.reply_to_message.sticker.is_video
     ):
-        return await txt.edit("Reply to a non-animated sticker")
+        return await txt.edit("𝖱𝖾𝗉𝗅𝗒 𝗍𝗈 𝖺 𝗇𝗈𝗇 𝖺𝗇𝗂𝗆𝖺𝗍𝖾𝖽 𝗌𝗍𝗂𝖼𝗄𝖾𝗋")
     if len(message.command) < 2:
         pack_name = f"{message.from_user.first_name}_sticker_pack_by_@app_Robot"
     else:
@@ -67,13 +67,13 @@ async def _packkang(app, message):
             )
         )
         await txt.edit(
-            f"""Your sticker has been added! For fast update remove your pack & add again\n
-🎖 𝗧𝗢𝗧𝗔𝗟 𝗦𝗧𝗜𝗖𝗞𝗘𝗥: {len(sticks)}""",
+            f"""𝖸𝗈𝗎𝗋 𝗌𝗍𝗂𝖼𝗄𝖾𝗋 𝗁𝖺𝗌 𝖻𝖾𝖾𝗇 𝖺𝖽𝖽𝖾𝖽! 𝖥𝗈𝗋 𝖿𝖺𝗌𝗍 𝗎𝗉𝖽𝖺𝗍𝖾 𝗋𝖾𝗆𝗈𝗏𝖾 𝗒𝗈𝗎𝗋 𝗉𝖺𝖼𝗄 𝖺𝗇𝖽 𝖺𝖽𝖽 𝖺𝗀𝖺𝗂𝗇\n
+╭•𝖳𝗈𝗍𝖺𝗅 𝖲𝗍𝗂𝖼𝗄𝖾𝗋𝗌: {len(sticks)}""",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
-                            "PACK", url=f"http://t.me/addstickers/{short_name}"
+                            "𝖯𝖺𝖼𝗄", url=f"http://t.me/addstickers/{short_name}"
                         )
                     ]
                 ]
