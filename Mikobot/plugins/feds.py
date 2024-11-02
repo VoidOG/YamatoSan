2562779c78b698ea6da7c3c10da84b8626db2a08
VoidOG <VoidGod123@outlook.com>
2024-11-02 14:20:51 +0530

    Update feds.py

--- Mikobot/plugins/feds.py
+++ Mikobot/plugins/feds.py
@@ -2464,16 +2464,16 @@ async def fed_user_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # <=================================================== HELP ====================================================>
 
 
-__mod_name__ = "FEDS"
+__mod_name__ = "Feds"
 
 __help__ = """
-➠ *Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts*.
+╭• *Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts*.
 *But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups*?\n
 *No longer!* *With Federation, you can make a ban in one chat overlap with all other chats*.\n
 *You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect*.\n
 
-➠ *Commands:*
-➠ Feds are now divided into 3 sections for your ease.
+╭• *Commands:*
+╭• Feds are now divided into 3 sections for your ease.
 
 » /fedownerhelp: Provides help for fed creation and owner only commands.
 
